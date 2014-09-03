f33c732c7f840c47f1d06d33ceaebbf5c58f7fde
Mats Roshauw <karmats@gmail.com>
2014-09-03 18:19:32 +0200

    Changed starting activity. Correct ws-address

--- app/src/main/java/com/geomarket/android/api/service/GeoMarketServiceApiBuilder.java
+++ app/src/main/java/com/geomarket/android/api/service/GeoMarketServiceApiBuilder.java
@@ -7,7 +7,7 @@ import retrofit.RestAdapter;
  */
 public class GeoMarketServiceApiBuilder {
 
-    static final String HOST = "http://192.168.2.102:9000";
+    static final String HOST = "http://109.74.2.250:9000";
 
     private GeoMarketServiceApiBuilder() {
     }
